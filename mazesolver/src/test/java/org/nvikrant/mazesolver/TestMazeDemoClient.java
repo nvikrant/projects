e9488d7f7be5fb73d8ae95b22a5b07beae2ad872
Vikrant Navalgund <vikrant_navalgund@live.com>
2013-09-22 12:16:56 +1000

    Adding a Maze Test Client.

---
+++ mazesolver/src/test/java/org/nvikrant/mazesolver/TestMazeDemoClient.java
@@ -0,0 +1,11 @@
+package org.nvikrant.mazesolver;
+
+/**
+ * Created by nvikrant on 9/19/13.
+ */
+public class TestMazeDemoClient {
+    /**
+     * Initialize the mazes with different algorithms
+     * and different grid sizes.
+     */
+}
